55b5692104b27272af66768e7f6b20bea9dec046
mpiriya <piriya.matt@gmail.com>
2021-04-14 11:24:17 -0400

    board check valid method

--- Board.java
+++ Board.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 public class Board {
-	int[][] values;
+	public int[][] values;
 	
 	public Board() {
 		values = new int[9][9];
@@ -16,6 +18,47 @@ public class Board {
 
 	public boolean valid() {
 		//row, col, box
+		int[] count = new int[10];
+		int i, j, k;
+		//row major order O(n^2)
+		for(i = 0; i < 9; i++) {
+			for(j = 0; j < 9; j++) 
+				count[values[i][j]]++; // count instances of each value
+			for(k = 1; k < count.length; k++) 
+				if(count[k] > 1) 
+					return false; // board not valid if any value appears more than once in a row
+			for(k = 0; k < count.length; k++) 
+				count[k] = 0;
+		}
+
+		//col major order O(n^2)
+		for(j = 0; j < 9; j++) {
+			for(i = 0; i < 9; i++) 
+				count[values[i][j]]++;
+			for(k = 1; k < count.length; k++) 
+				if(count[k] > 1) 
+					return false; // board not valid if any value appears more than once in a col
+			for(k = 0; k < count.length; k++) 
+				count[k] = 0;			
+		}
+
+		//go thru each box
+		int br, bc, rowLim, colLim;
+		for(i = 0; i < 3; i++) {
+			for(j = 0; j < 3; j++) {
+				rowLim = i*3 + 3;
+				colLim = j*3 + 3;
+				for(br = i*3; br < rowLim; br++) 
+					for(bc = j*3; bc < colLim; bc ++) 
+						count[values[br][bc]]++;
+				for(k = 1; k < count.length; k++) 
+					if(count[k] > 1) 
+						return false; // board not valid if any value appears more than once in a box
+				for(k = 0; k < count.length; k++) 
+					count[k] = 0;	
+			}
+		}
+
 		return true;
 	}
 
@@ -48,5 +91,27 @@ public class Board {
 
 		Board b = new Board(values);
 		System.out.println(b);
+	}
+}
+
+class Tile {
+	int value;
+	int row;
+	int col;
+	ArrayList<Integer> possible;
+
+	public Tile(int row, int col, ArrayList<Integer> possible) {
+		this.value = -1;
+		this.row = row;
+		this.col = col;
+		this.possible = possible;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public void setPossible(ArrayList<Integer> possible) {
+		this.possible = possible;
 	}
 }
